02da4d9289de38c0f98cfacd413ce1100e40898c
subbamaggus <markus.a.kerle@gmail.com>
2025-11-09 08:13:26 +0100

    split SPA, rename files more generic

--- www/add_entry.php
+++ www/add_entry.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
   <link href="main.css" rel="stylesheet">
-  <title>Vereinsabrechnung</title>
+  <title>Add New Entry - Vereinsabrechnung</title>
   <script src="https://unpkg.com/vue@3/dist/vue.global.prod.js"></script>
 </head>
 <body>
 
-  <div id="app"></div>
-  <script src="index.js"></script>
+  <div id="add-entry-app"></div>
+  <script src="add_entry.js"></script>
   
 </body>
 </html>
--- www/index.js
+++ www/index.js
@@ -9,13 +9,6 @@ const app = Vue.createApp({
       email: '',
       password: '',
       isLogin: true,
-      showAddEntry: false,
-      newEntry: {
-        name: '',
-        value: '',
-        date: new Date().toISOString().slice(0, 10),
-        myimage: null,
-      },
       data: [],
       years: [],
       currentYear: 0,
@@ -94,44 +87,6 @@ const app = Vue.createApp({
         } finally {
             window.location.reload();
         }
-    },
-    async storeEntry() {
-        const formData = new FormData();
-        formData.append('name', this.newEntry.name);
-        formData.append('value', this.newEntry.value);
-        formData.append('date', this.newEntry.date);
-        if (this.newEntry.myimage) {
-            formData.append('myimage', this.newEntry.myimage);
-        }
-
-        try {
-            const response = await fetch('api.php?method=store_entry', {
-                method: 'POST',
-                body: formData,
-            });
-
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.error || 'Failed to add entry');
-            }
-
-            const result = await response.json();
-            if (result.success) {
-                this.showAddEntry = false;
-                this.newEntry.name = '';
-                this.newEntry.value = '';
-                this.newEntry.myimage = null;
-                this.$refs.fileInput.value = '';
-                this.fetchData();
-            } else {
-                throw new Error('Failed to add entry');
-            }
-        } catch (e) {
-            this.error = e;
-        }
-    },
-    handleFileUpload(event) {
-        this.newEntry.myimage = event.target.files[0];
     },
     async fetchMandanten() {
         try {
@@ -467,29 +422,7 @@ const app = Vue.createApp({
             </span>
         </div>
         <button @click="logout">Logout</button>
-        <button @click="showAddEntry = !showAddEntry">{{ showAddEntry ? 'Cancel' : 'Add Entry' }}</button>
-        <div v-if="showAddEntry">
-            <h2>Add New Entry</h2>
-            <form @submit.prevent="storeEntry">
-                <label>Betrag<br>
-                    <input type="number" step="0.01" v-model="newEntry.value">
-                </label>
-                <br>
-                <label>Datum<br>
-                    <input type="date" v-model="newEntry.date">
-                </label>
-                <br>
-                <label>Bezeichnung<br>
-                    <input type="text" v-model="newEntry.name">
-                </label>
-                <br>
-                <label>Bild<br>
-                    <input type="file" @change="handleFileUpload" ref="fileInput">
-                </label>
-                <br>
-                <button type="submit">speichern</button>
-            </form>
-        </div>
+        <a href="add_entry.php" class="button">Add Entry</a>
         <div>
 
         </div>
