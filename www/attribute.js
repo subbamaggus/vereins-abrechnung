89de96b0d991bba45a7f3c39521c4af54616c779
subbamaggus <markus.a.kerle@gmail.com>
2025-11-09 09:35:11 +0100

    structure showing as wanted

--- www/attribute.js
+++ www/attribute.js
@@ -29,24 +29,19 @@ const attributeApp = Vue.createApp({
 
       <h1>Manage Attributes</h1>
       
-        <div v-if="attributes.length" style="margin-bottom: 10px;">
 
-            <div v-for="item in years" :key="item.id">
-                <input type="radio" :id="'year-' + item.year" name="yearPicked" :value="item.year" v-model="currentYear" @change="clickYear(item.year)" >
-                <label :for="'year-' + item.year">{{ item.year }}</label>
-            </div>
-            <div style="margin-bottom: 10px;">
-                <strong>Filter by:</strong>
-                <div v-for="group in attributes" :key="group.id" style="margin-bottom: 5px;">
-                    <strong>{{ group.name }}:</strong>
-                    <label v-for="attr in group.attribute" :key="attr.id" style="margin-right: 10px; margin-left: 5px;">
-                        <input type="checkbox" :value="attr.id" v-model="selectedFilters"> {{ attr.name }}
-                    </label>
-                </div>
-                <button @click="applyFilters">Apply Filters</button>
-            </div>
-
+      <div style="margin-bottom: 10px;">
+        <div v-for="group in attributes" :key="group.id" style="margin-bottom: 5px;">
+          <strong>{{ group.name }}:</strong>
+          <br/>
+          <label v-for="attr in group.attribute" :key="attr.id" style="margin-right: 10px; margin-left: 5px;">
+            <input type="input" :value="attr.id" v-model="selectedFilters" value="{{ attr.name }}"</input>
+            <br/>
+          </label>
+          <a href="#">new attribute</a>
         </div>
+        <a href="#">new attribute group</a>
+      </div>
 
       <p v-if="error" style="color: red;">{{ error }}</p>
       <p v-if="success" style="color: green;">Entry added successfully!</p>
