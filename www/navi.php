b4512c0f5e685a91c59ba3e64b102ce2b4b346db
subbamaggus <markus.a.kerle@gmail.com>
2025-11-09 15:08:27 +0100

    also add navi

---
+++ www/navi.php
@@ -0,0 +1,5 @@
+  <a href="index.php">Home</a>&nbsp;
+  <a href="add_entry.php">Add Entry</a>&nbsp;
+  <a href="attribute.php">Attribute</a>&nbsp;
+  <a href="depot.php">Depot</a>&nbsp;
+  <a href="mandant.php">Mandant</a>&nbsp;
