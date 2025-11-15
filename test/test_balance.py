a18084dd2b66e67349502c2b4c58a83f5182655b
subbamaggus <markus.a.kerle@gmail.com>
2025-11-15 18:28:10 +0100

    test prepared. needs result checking

--- test/test_balance.py
+++ test/test_balance.py
@@ -1,5 +1,21 @@
 import os
+import json
 
 data = os.popen("curl http://localhost/v-a/www/api.php?method=get_balance'&'apikey=345678").read()
 
-print(data)
+json_object = json.loads(data)
+
+print(json_object["items"])
+
+sum = 0
+
+for element in json_object["items"]:
+    sum = sum + float (element["value"])
+
+mydiff = 0
+for element in json_object["depots"]:
+    diff = float(element['end'] - element['start'])
+    mydiff = mydiff + diff
+
+if mydiff == sum:
+    print("yes")
